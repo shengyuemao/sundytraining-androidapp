c487b1c3a7d6af8a417e0f736719b3ec77972c7f
sundy zhang <sundyzlh@gmail.com>
2011-09-22 23:07:52 +0800

    Demo Init

---
+++ SundyAndroidDemo_AA001/src/sundy/android/demo/activityandintent/ThirdActivity.java
@@ -0,0 +1,17 @@
+package sundy.android.demo.activityandintent;
+
+import sundy.android.demo.R;
+import sundy.android.demo.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ThirdActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_activity3)  ;
+	}
+
+}
